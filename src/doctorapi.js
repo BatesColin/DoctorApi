cdb14174a6bf8b626b71c25051b72c42251ef82c
cb <dragonflylord@gmail.com>
2018-09-02 20:35:37 -0700

    DoctorApi will now have untested code that should make api calls and put them on the index page.

--- src/doctorapi.js
+++ src/doctorapi.js
@@ -2,13 +2,13 @@ export class Doctor{
     constructor(){
   
     }
-    static getData(query, hi, error){
+    static getData(name, condition, result, error){
         $.get(`https://api.betterdoctor.com/2016-03-01/doctors?
         name=${name}&query=
-        ${query}&location=wa-seattle&user_location=37.773%2C-122.413&sort=best-match-desc&skip=0&limit=100&
+        ${condition}&location=wa-seattle&user_location=37.773%2C-122.413&sort=best-match-desc&skip=0&limit=100&
         user_key=${process.env.exports.apiKey}`)
         .then(function(response){
-          hi(response);
+          result(response);
         }).fail(function(){
           error();
         });
